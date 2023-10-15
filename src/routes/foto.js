83a9d98967c0f87ca6466ad8b02f170cb42f668c
Matheus Dario <dario.matheus97@gmail.com>
2023-10-15 17:46:05 -0300

    Feature tratando os dados do Upload

--- src/routes/foto.js
+++ src/routes/foto.js
@@ -1,12 +1,9 @@
 import { Router } from 'express';
-import multer from 'multer';
+
 import fotoController from '../controllers/Foto';
-import multerConfig from '../config/multer';
 
 const router = new Router();
 
-const upload = multer(multerConfig);
-
-router.post('/', upload.single('foto'), fotoController.store);
+router.post('/', fotoController.store);
 
 export default router;
