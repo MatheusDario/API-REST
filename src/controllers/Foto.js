b083c1b059af6926bd4681f4cef3e1930d1435f3
Matheus Dario <dario.matheus97@gmail.com>
2023-10-15 18:49:45 -0300

    Salvando a foto na BD

--- src/controllers/Foto.js
+++ src/controllers/Foto.js
@@ -1,17 +1,29 @@
 import multer from 'multer';
 import multerConfig from '../config/multer';
+import FotoModel from '../models/Foto';
 
 const upload = multer(multerConfig).single('foto');
 
 class Foto {
-  async store(req, res) {
-    return upload(req, res, (err) => {
+  store(req, res) {
+    return upload(req, res, async (err) => {
       if (err) {
         return res.status(400).json({
           errors: [err.code],
         });
       }
-      return res.json(req.file);
+
+      try {
+        const { originalname, filename } = req.file;
+        const { aluno_id } = req.body;
+        const foto = await FotoModel.create({ originalname, filename, aluno_id });
+
+        return res.json(foto);
+      } catch (e) {
+        return res.status(400).json({
+          errors: ['Aluno não existe'],
+        });
+      }
     });
   }
 }
---
+++ src/database/migrations/20231015205916-criar-tabela-foto-do-aluno.js
@@ -0,0 +1,43 @@
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('fotos', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      originalname: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      filename: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      aluno_id: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+        references: {
+          model: 'alunos',
+          key: 'id',
+        },
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  async down(queryInterface) {
+    await queryInterface.dropTable('fotos');
+  },
+};
