e7cb3d7ace6b2bf33b3349f4748a7271fc805b19
Matheus Dario <dario.matheus97@gmail.com>
2023-10-15 20:15:24 -0300

    deploy

---
+++ dist/models/Aluno.js
@@ -0,0 +1,71 @@
+"use strict";Object.defineProperty(exports, "__esModule", {value: true}); function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _sequelize = require('sequelize'); var _sequelize2 = _interopRequireDefault(_sequelize);
+
+ class Aluno extends _sequelize.Model {
+  static init(sequelize) {
+    super.init({
+      nome: {
+        type: _sequelize2.default.STRING,
+        defaultValue: '',
+        validate: {
+          len: {
+            args: [3, 255],
+            msg: 'Campo nome deve ter entre 3 e 255 caracteres',
+          },
+        },
+      },
+      sobrenome: {
+        type: _sequelize2.default.STRING,
+        defaultValue: '',
+        validate: {
+          len: {
+            args: [3, 255],
+            msg: 'Campo sobrnome deve ter entre 3 e 255 caracteres',
+          },
+        },
+      },
+      email: {
+        type: _sequelize2.default.STRING,
+        defaultValue: '',
+        validate: {
+          isEmail: {
+            msg: 'Por favor digite um E-mail válido',
+          },
+        },
+      },
+      idade: {
+        type: _sequelize2.default.INTEGER,
+        defaultValue: '',
+        validate: {
+          isInt: {
+            msg: 'Campo idade deve ter um número inteiro',
+          },
+        },
+      },
+      peso: {
+        type: _sequelize2.default.FLOAT,
+        defaultValue: '',
+        validate: {
+          isFloat: {
+            msg: 'Campo peso deve ter um número inteiro ou número com casas decimais',
+          },
+        },
+      },
+      altura: {
+        type: _sequelize2.default.FLOAT,
+        defaultValue: '',
+        validate: {
+          isFloat: {
+            msg: 'Campo peso deve ter um número inteiro ou número com casas decimais',
+          },
+        },
+      },
+    }, {
+      sequelize,
+    });
+    return this;
+  }
+
+  static associate(models) {
+    this.hasMany(models.Foto, { foreignKey: 'aluno_id' });
+  }
+} exports.default = Aluno;
