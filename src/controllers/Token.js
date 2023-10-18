677c4174b70fb3793d7f8e8e09f7d89587287f61
Matheus Dario <dario.matheus97@gmail.com>
2023-10-17 22:07:42 -0300

    Add cors

--- src/controllers/Token.js
+++ src/controllers/Token.js
@@ -30,7 +30,7 @@ class Home {
       expiresIn: process.env.TOKEN_EXPIRATION,
     });
 
-    return res.json({ token });
+    return res.json({ token, user: { nome: user.nome, id, email } });
   }
 }
 
